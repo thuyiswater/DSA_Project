86ef0c30759d007db1f67afa86bedbf358119864
DieuLam <you@example.com>
2022-04-14 14:08:53 +0700

    copy data file

---
+++ BinarySearch.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class BinarySearch {
+
+    public void Search(int[] arr, int target) {
+
+        int left = arr[0];
+        int right = arr[arr.length - 1];
+        int mid = arr[arr.length / 2];  // calculate middle value
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
