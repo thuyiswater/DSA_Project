1415f1085aa57bd013cde65f57aa9970136c9846
minhtorang <s3878058@rmit.edu.vn>
2022-04-14 23:04:29 +0700

    add comment

--- DataHandler.java
+++ DataHandler.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 public class DataHandler {
     public static void main(String[] args) throws Exception {
+    
+        //test functions
         MyArrayList<String> myArr = new MyArrayList<String>();
         myArr.add("1");
         myArr.add("2");
@@ -12,24 +14,27 @@ public class DataHandler {
             System.out.println(myArr.get(i));
         }
 
+        //read and write file nhưng đang đợi chị Thủy push class Customer lên mới làm típ đc
         BufferedReader reader = new BufferedReader(new FileReader("data.csv"));
-        reader.readLine(); // skip 1st line
         String line = reader.readLine();
         
         reader.close();
     }
 }
 
+// implement ArrayList
 class MyArrayList<T> {
     private Object[] elements;
     private int size;
-    private static final int DEFAULT_CAPACITY = 1000;
+    private static int DEFAULT_CAPACITY = 1000; // temporary capacity of array when first created
 
+    // constructor
     public MyArrayList() {
         elements = (T[]) new Object[DEFAULT_CAPACITY];
         size = 0;
     }
 
+    // method: add element to the last position
     public void add(T obj) {
         if (size > elements.length) {
             increaseCapa();
@@ -38,6 +43,7 @@ class MyArrayList<T> {
         size++;
     }
 
+    // method: add element to a specific position
     public void add(int index, T obj) {
         if (size > elements.length) {
             increaseCapa();
@@ -49,6 +55,7 @@ class MyArrayList<T> {
         size++;
     }
 
+    // method: return element of a specific position
     public T get(int index) throws Exception {
         if(index < size){
             return (T) elements[index];
@@ -57,11 +64,13 @@ class MyArrayList<T> {
         }
     }
 
+    // automatically increase capacity of array when full
     private void increaseCapa() {
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
 
+    // return number of element in arrays, not full capacity
     public int size() {
         return size;
     }
