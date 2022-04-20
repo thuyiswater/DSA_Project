e2ae6daa04b1c1a06a30a2065d600a6e3f2ad2fd
thuy <s3877746@rmit.edu.vn>
2022-04-20 09:35:53 +0700

    menu bar

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\t\t\t\t\t Welcome to Record Management for Intelligent Tracking (RMIT)");
+        System.out.println("Menu: ");
+        System.out.printf("1. Add a new customer\n" +
+                "2. Update an existing customer\n" +
+                "3. Search for one customer\n" +
+                "4. Search for a list of customer\n");
+        // System.out.printf("Please select an option by enter the number: ");
+        
+    }
+}
